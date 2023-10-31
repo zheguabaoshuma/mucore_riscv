953f8a17502d56cef2e45b227860d184b8ec2327
Tuuuu <2010920@mail.nankai.edu.cn>
2023-11-01 00:40:24 +0800

    sync

--- lab3/kern/mm/swap_lru.c
+++ lab3/kern/mm/swap_lru.c
@@ -24,7 +24,10 @@ _page_unregister(struct Page *page) {
 }
 static int
 _page_use(uintptr_t pra_vaddr) {
-    lru_use[(int)(pra_vaddr/PGSIZE)]++;
+    cprintf("pra_vaddr %x comes into use\n",pra_vaddr);
+    if(lru_use[(int)(pra_vaddr/PGSIZE)]==9999)
+        lru_use[(int)(pra_vaddr/PGSIZE)]=1;
+    else lru_use[(int)(pra_vaddr/PGSIZE)]++;
     return 0;
 }
 
@@ -47,7 +50,7 @@ static int
 _lru_init_mm(struct mm_struct* mm) {
     list_init(&pra_list_head);
     mm->sm_priv=&pra_list_head;
-    for(int i=1;i<100;i++){
+    for(int i=0;i<100;i++){
         lru_use[i]=9999;
     }
     return 0;
@@ -59,7 +62,7 @@ _lru_map_swappable(struct mm_struct* mm, uintptr_t addr, struct Page* page, int 
     list_entry_t* entry=&(page->pra_page_link);
     assert(entry!=NULL && head!=NULL);
     list_add(head, entry);
-    _page_register(page);
+    //_page_register(page);
     return 0;
 }
 
@@ -80,10 +83,21 @@ _lru_swap_out_victim(struct mm_struct *mm, struct Page ** ptr_page, int in_tick)
             min_index=i;
         }
     }
-    
+    cprintf("min_index %d min %d\n",min_index,min);
+
+    list_entry_t *lep = head->next;
+    while(1){
+        if (lep == head) {
+            break;
+        }
+        struct Page *pt = le2page(lep, pra_page_link);
+        cprintf("p->pra_vaddr %x, index %d, use_times %d \n",pt->pra_vaddr, (int)(pt->pra_vaddr/PGSIZE), lru_use[(int)(pt->pra_vaddr/PGSIZE)]);
+        lep = lep->next;
+    }
+
     while (1) {
         struct Page *p = le2page(le, pra_page_link);
-        cprintf("p->pra_vaddr %x, index %d, use_times %d \n",p->pra_vaddr, (int)(p->pra_vaddr/PGSIZE), lru_use[(int)(p->pra_vaddr/PGSIZE)]);
+        //cprintf("p->pra_vaddr %x, index %d, use_times %d \n",p->pra_vaddr, (int)(p->pra_vaddr/PGSIZE), lru_use[(int)(p->pra_vaddr/PGSIZE)]);
         assert(p != NULL);
         if (p->pra_vaddr == (uintptr_t)(min_index)*PGSIZE) {
             *ptr_page = p;
@@ -119,31 +133,40 @@ _lru_check_swap(void){
     assert(pgfault_num==5);
     *(unsigned char *)0x1000 = 0x0a;
     _page_use(0x1000);
-    assert(pgfault_num==5);
+    assert(pgfault_num==6);
     *(unsigned char *)0x2000 = 0x0b;
     _page_use(0x2000);
-    assert(pgfault_num==5);
+    assert(pgfault_num==6);
     *(unsigned char *)0x2000 = 0x0b;
     _page_use(0x2000);
-    assert(pgfault_num==5);
+    assert(pgfault_num==6);
     *(unsigned char *)0x2000 = 0x0b;
     _page_use(0x2000);
-    assert(pgfault_num==5);
+    assert(pgfault_num==6);
     *(unsigned char *)0x2000 = 0x0b;
     _page_use(0x2000);
-    assert(pgfault_num==5);
+    assert(pgfault_num==6);
     *(unsigned char *)0x3000 = 0x0c;
     _page_use(0x3000);
-    assert(pgfault_num==5);
+    assert(pgfault_num==7);
     *(unsigned char *)0x4000 = 0x0d;
     _page_use(0x4000); 
-    assert(pgfault_num==5);
+    assert(pgfault_num==7);
     *(unsigned char *)0x5000 = 0x0e;
     _page_use(0x5000);
-    assert(pgfault_num==5);
+    assert(pgfault_num==7);
     *(unsigned char *)0x1000 = 0x0a;
     _page_use(0x1000);
-    assert(pgfault_num==6);
+    assert(pgfault_num==8);
+    *(unsigned char *)0x1000 = 0x0a;
+    _page_use(0x1000);
+    assert(pgfault_num==8);
+    *(unsigned char *)0x4000 = 0x0a;
+    _page_use(0x6000);
+    assert(pgfault_num==8);
+    *(unsigned char *)0x1000 = 0x0a;
+    _page_use(0x1000);
+    assert(pgfault_num==8);
 
     return 0;
 }
